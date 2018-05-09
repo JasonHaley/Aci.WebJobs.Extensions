6f4219cf7ac1d603cf3074efe94fb99f43ef31db
Jason Haley <jason@jasonhaley.com>
2018-05-09 06:38:21 -0700

    cleaned up sample

--- src/Aci.WebJobs.Extensions/Samples/WebJobApp/Functions.cs
+++ src/Aci.WebJobs.Extensions/Samples/WebJobApp/Functions.cs
@@ -9,10 +9,10 @@ namespace WebJobApp
     public static class Functions
     {
         public static async Task StartContainerInstance([QueueTrigger("create-aci")]string startQueue,
-               [Aci(ContainerImageName = "bkimminich/juice-shop", Port = 3000, AciName = "juice-shop", Action = AciAction.Create)] AciClient aciClient,
+               [Aci(AciName = "juice-shop", Action = AciAction.Create)] AciClient aciClient,
                TraceWriter log)
         {
-            log.Info($"C# Queue trigger function processed: {startQueue}");
+            log.Info("Creating container instance...");
 
             await aciClient.CreateAsync("bkimminich/juice-shop", 3000);
 
@@ -30,17 +30,15 @@ namespace WebJobApp
             [Aci(AciName = "juice-shop", Action = AciAction.Delete)] AciClient aciClient,
             TraceWriter log)
         {
-            log.Info($"C# Queue trigger function processed: {deleteQueue}");
+            var logs = await aciClient.GetLogContentAsync();
+            log.Info($"Container logs: {logs}");
+
+            log.Info($"Deleting container...");
 
             await aciClient.DeleteAsync();
+
+            log.Info("Container deleted.");
         }
-
-        //[FunctionName("GetLogsContainerInstance")]
-        //public static void GetLogsContainerInstance([TimerTrigger("00:00:30")] TimerInfo timer,
-        //    [Aci(Action = AciAction.Logs)] AciClient client,
-        //    TraceWriter log)
-        //{
-
-        //}
+        
     }
 }
